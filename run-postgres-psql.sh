8f62083ccba9c001bdbec989af5e8ad039dfe671
Lucas Russo <lerwys@gmail.com>
2016-08-22 23:05:11 -0300

    run-postgres-psql.sh: use default user postgres

--- run-postgres-psql.sh
+++ run-postgres-psql.sh
@@ -9,4 +9,4 @@ DNS_IP="$2"
 
 # Run postgres
 docker run --name naming-service-postgres-psql --net ${NET_NAME} --dns ${DNS_IP} \
-    -it postgres psql -h naming-service-postgres -U naming-service-postgres
+    -it postgres psql -h naming-service-postgres -U postgres
