2135a834e5e653da16abacdb33b773d080c70843
Lucas Russo <lerwys@gmail.com>
2016-09-05 15:54:49 -0300

    env-vars.sh: use same network name for all DISCS services

--- env-vars.sh
+++ env-vars.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-NET_NAME="postgres"
+#NET_NAME="postgres"
+NET_NAME="dockerrbacauthservicescomposed_postgres-rbac"
 DNS_IP="10.0.0.72"
 POSTGRES_PORT=5432
 POSTGRES_USER="discs_names"
